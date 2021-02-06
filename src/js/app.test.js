a8d01499c6e21f09d9776ac48785ff4cd87077b7
Alex Smith <alex.vesson.smith@gmail.com>
2021-02-06 15:43:59 +0300

    lint fix

--- src/js/app.test.js
+++ src/js/app.test.js
@@ -1,6 +1,6 @@
 import GameSavingLoader from './game-saving-loader';
 
-test ('return string', async () => {
+test('return string', async () => {
   const data = await GameSavingLoader.load();
   expect(data).toBe('{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}');
-})
+});
